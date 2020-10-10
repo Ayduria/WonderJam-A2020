c7be7a66fdd836fa6c8a207af1997833016e0466
Christopher Harvey <webharveyc@gmail.com>
2020-10-09 22:07:02 -0400

    Base du jeu

---
+++ Assets/Scripts/InstantiatePrefabs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePrefabs : MonoBehaviour
+{
+
+    private GameObject[] prefabs;
+    public int multiplier = 60;
+
+    public Transform spawnerPosition;
+ 
+    void Start(){
+
+        prefabs = Resources.LoadAll<GameObject>("Ingredients");
+
+        for(int v = 0; v < prefabs.Length; v++){
+            Debug.Log(prefabs[v]);
+        }
+
+        for (int i = 0; i < multiplier; i++)
+        {  
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], transform.position, transform.rotation);
+        }
+        
+    }
+    
+    void Update()
+    {
+       
+    }
+
+
+}
