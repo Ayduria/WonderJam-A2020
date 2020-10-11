67908f7f82845176515045c697254bd0e12deb86
Christopher Harvey <webharveyc@gmail.com>
2020-10-10 21:06:45 -0400

    Ajout du spitter

---
+++ Assets/Spitter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Spitter : MonoBehaviour
+{
+
+    public GameObject prefabs;
+    public float numberOfMonsters = 5;
+    private float currentMonstersNumber;
+    public float Timer = 0.1f;
+    public float spitSpeed = 2000;
+    public float spitOffset= 1000;
+
+    public Transform spawnerPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0f)
+            {
+
+                if(currentMonstersNumber <  numberOfMonsters){
+                    GameObject mob = Instantiate(prefabs, spawnerPosition.transform.position + prefabs.transform.localScale , transform.rotation);
+                    mob.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(spitOffset * -1, spitOffset) * Time.deltaTime, spitSpeed * Time.deltaTime);
+                    Timer = 0.1f;
+                }
+
+                currentMonstersNumber ++;
+
+            }
+        
+            
+    }
+}
---
+++ Assets/getIngredients.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class getIngredients : MonoBehaviour
+{
+
+    public GameObject[] ingredients;
+    public int[] ingredientsQuantity;
+    private float malus = 0;
+    private bool ingredientExist = false;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         for (int i = 0; i < ingredients.Length; i++)
+        {
+            Debug.Log(ingredients[i]);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D ingredientColliding) {
+  
+
+          
+        for (int i = 0; i < ingredients.Length; i++)
+        {
+            var trimmedName = ingredientColliding.gameObject.name.Replace("(clone)","");
+
+            if(ingredientColliding.gameObject.name.Replace("(Clone)","") == ingredients[i].name){
+                ingredientExist = true;
+            }
+        } 
+
+        
+
+        if(ingredientExist == false){
+            malus++;
+            Debug.Log(malus);
+        }
+        else{
+            ingredientExist = false;
+        }
+
+    }
+}
